e80d7fd0c1c8ce02757ca9e0f59d4c29e3e52957
andy <andy7d0@gmail.com>
2026-03-26 10:27:50 +0300

    redesign controls

--- src/web/cmn/questions-input.jsx
+++ src/web/cmn/questions-input.jsx
@@ -71,8 +71,10 @@ export default function QInput({meta, value, valSetter }) {
 			case '/': setR('+/-'); break;
 			case 'ArrowUp': case '+': case '=': setR('+'); break;
 			case 'ArrowDown': case '-': case '_': setR('-'); break;
+			case 'Home': setNpp(0,true); break;
+			case 'End':  setNpp(arr.length+1); break;
 			case 'ArrowLeft': setNpp(n=>n>1? n-1 : n); break;
-			case 'ArrowRight': setNpp(n=>n<arr.length? n+1 : n); break;
+			case 'ArrowRight': setNpp(n=>n<=arr.length? n+1 : n); break;
 			}
 		}
 		window.addEventListener('keydown', keydown)
@@ -80,76 +82,93 @@ export default function QInput({meta, value, valSetter }) {
 	},[setNpp, setR, q, arr])
 
 	return <div>
-		{ npp === '' && <div>
+		{ !npp && <div>
 				<button type="button" onClick={()=>{setNpp(1, true)}}
 						style={{position:"fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}}
 				>Начать</button>
 			</div>
 		}
-		{ typeof npp === 'number' &&<div>
-			<div style={{float:"right", marginRight:"1em"}}><button type="button" onClick={()=>setNpp('', true)}>H</button></div>
+		{ npp === arr?.length+1 && <div>
+				<button type="button" 
+						style={{position:"fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}}
+				>Завершить заполнение и отправить результаты</button>
+			</div>
+		}
+		{ !!npp && npp <= arr.length &&<div>
 			<h6>{q.lvl}</h6>
 			<h4>{q.hdr}</h4>
 			<h3>{q.h}</h3>
 			<div><i>{q.t}</i></div>
+		</div>}
 			<div
-				style={{position:"fixed", bottom:"4em"}}
+				style={{position:"fixed"
+					, bottom:"0em"
+					, background: "white"
+					, height: "12em"
+					, width: 380
+					, left: "50%"
+					, transform: "translate(-50%,0)"
+					}}
 			>
-				<table><tbody>
-				<tr><td><input type="radio" onChange={()=>setR('+')}
-						checked={r === '+'}
-					/> 
-					</td>
-					<td>
-					<button type="button" onClick={()=>setR('+')} style={bstyle}> 
-					навык есть в 80 и более % случаев, работа над навыком не требуется
-					</button>
-					</td>
-				</tr>
-				<tr><td><input type="radio" onChange={()=>setR('+/-')}
-						checked={r === '+/-'}
-					/> 
-					</td>
-					<td>
-					<button type="button" onClick={()=>setR('+/-')} style={bstyle}>
-					навык в процессе формирования, иногда есть, иногда нет
-					</button>
-					</td>
-				</tr>
-				<tr><td><input type="radio" onChange={()=>setR('-')}
-						checked={r === '-'}
-					/> 
-					</td>
-					<td>
-					<button type="button" onClick={()=>setR('-')} style={bstyle}>
-					навыка практически или совсем нет
-					</button>
-					</td>
-				</tr>
-				<tr><td><input type="radio" onChange={()=>setR(null)}
-						checked={r === null}
-					/>
-					</td>
-					<td>
-					<button type="button" onClick={()=>setR(null)} style={bstyle}>
-					нельзя оценить навык и/или нет возможности проверить
-					</button>
-					</td>
-				</tr>
-				</tbody></table>
-			</div>
-			<div>
-				{npp>1&&<button type="button" onClick={()=>{setNpp(n=>n-1)}}
-						style={{position:"fixed", left: "1em", bottom: "1em"}}
+			{npp && npp<=arr.length && <table style={{width:"100%"}}><tbody>
+			<tr><td><input type="radio" onChange={()=>setR('+')}
+					checked={r === '+'}
+				/> 
+				</td>
+				<td>
+				<button type="button" onClick={()=>setR('+')} style={bstyle}> 
+				навык есть в 80 и более % случаев, работа над навыком не требуется
+				</button>
+				</td>
+			</tr>
+			<tr><td><input type="radio" onChange={()=>setR('+/-')}
+					checked={r === '+/-'}
+				/> 
+				</td>
+				<td>
+				<button type="button" onClick={()=>setR('+/-')} style={bstyle}>
+				навык в процессе формирования, иногда есть, иногда нет
+				</button>
+				</td>
+			</tr>
+			<tr><td><input type="radio" onChange={()=>setR('-')}
+					checked={r === '-'}
+				/> 
+				</td>
+				<td>
+				<button type="button" onClick={()=>setR('-')} style={bstyle}>
+				навыка практически или совсем нет
+				</button>
+				</td>
+			</tr>
+			<tr><td><input type="radio" onChange={()=>setR(null)}
+					checked={r === null}
+				/>
+				</td>
+				<td>
+				<button type="button" onClick={()=>setR(null)} style={bstyle}>
+				нельзя оценить навык и/или нет возможности проверить
+				</button>
+				</td>
+			</tr>
+			</tbody></table>}
+			<div style={{position:"absolute", bottom:10, width:"100%"}}>
+				{npp>=1&&<button type="button" onClick={()=>{setNpp(0,true)}}
+						style={{float:"left"}}
+					> |&lt; </button>}
+				{npp>=1&&<button type="button" onClick={()=>{setNpp(n=>n-1)}}
+						style={{float:"left"}}
 					> &lt;-- </button>}
-				{npp && <span
-					style={{position:"fixed", bottom: "1em", left:"50%", transform:"translate(-50%,0)"}}
-					>{npp}/{arr.length}</span>}
-				{npp<flat.length&&<button type="button" onClick={()=>{setNpp(n=>n+1)}}
-						style={{position:"fixed", right: "1em", bottom: "1em"}}
+				{npp<=arr.length&&<button type="button" onClick={()=>{setNpp(arr.length+1)}}
+						style={{float:"right"}}
+				> &gt;| </button>}
+				{npp<=arr.length&&<button type="button" onClick={()=>{setNpp(n=>n+1)}}
+						style={{float:"right"}}
 				> --&gt; </button>}
+				{npp && npp <= arr.length && <span
+					style={{position:"absolute", bottom: 0, left:"50%", transform:"translate(-50%,0)"}}
+					>{npp}/{arr.length}</span>}
 			</div>
 		</div>
-		}
 	</div>
 }
